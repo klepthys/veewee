27fb822dd95c6cd30bded323e22cffbfdb898c6e
Sebastien Dumetz <sebastien.dumetz@isen-lille.fr>
2014-03-06 13:40:40 +0100

    now installing sudo

--- definitions/holusion-base/base.sh
+++ definitions/holusion-base/base.sh
@@ -1,7 +1,8 @@
 # Update the box
+#Everything here will probably be already installed but it doesn't hurt to check...
 apt-get -y update
 apt-get -y install linux-headers-$(uname -r) build-essential
-apt-get -y install zlib1g-dev libssl-dev libreadline-gplv2-dev
+apt-get -y install zlib1g-dev libssl-dev libreadline-gplv2-dev sudo
 apt-get -y install curl unzip
 
 # Set up sudo
@@ -11,6 +12,7 @@ echo 'vagrant ALL=NOPASSWD:ALL' > /etc/sudoers.d/vagrant
 echo 'UseDNS no' >> /etc/ssh/sshd_config
 
 # Remove 5s grub timeout to speed up booting
+#Should be done by default install...
 cat <<EOF > /etc/default/grub
 # If you change this file, run 'update-grub' afterwards to update
 # /boot/grub/grub.cfg.
