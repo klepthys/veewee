d8b16af21d73637ef745a73a3a128936555fbae4
Sebastien Dumetz <sebastien.dumetz@isen-lille.fr>
2014-03-06 13:39:54 +0100

    changed home dir for vagrant user from /home/vagrant to /vagrant

--- definitions/holusion-base/vagrant.sh
+++ definitions/holusion-base/vagrant.sh
@@ -3,19 +3,19 @@
 date > /etc/vagrant_box_build_time
 
 # Create the user vagrant with password vagrant
-useradd -G sudo -p $(perl -e'print crypt("vagrant", "vagrant")') -m -s /bin/bash -N vagrant
+useradd -G sudo -p $(perl -e'print crypt("vagrant", "vagrant")') -m -s /bin/bash -N vagrant -d /vagrant
 
 # Install vagrant keys
-mkdir -pm 700 /home/vagrant/.ssh
-curl -Lo /home/vagrant/.ssh/authorized_keys \
+mkdir -pm 700 /vagrant/.ssh
+curl -Lo /vagrant/.ssh/authorized_keys \
   'https://raw.github.com/mitchellh/vagrant/master/keys/vagrant.pub'
-chmod 0600 /home/vagrant/.ssh/authorized_keys
-chown -R vagrant:vagrant /home/vagrant/.ssh
+chmod 0600 /vagrant/.ssh/authorized_keys
+chown -R vagrant:vagrant /vagrant/.ssh
 
 # Customize the message of the day
 echo 'Welcome to your Vagrant-built virtual machine.' > /var/run/motd
 
 # Install NFS client
-apt-get -y install nfs-common
+#apt-get -y install nfs-common
 
 
