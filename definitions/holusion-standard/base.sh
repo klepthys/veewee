599d50012992663a54c71ed16db542a0f77aecfd
Sebastien Dumetz <sebastien.dumetz@isen-lille.fr>
2014-03-10 09:52:32 +0100

    added a standard definition

---
+++ definitions/holusion-standard/base.sh
@@ -0,0 +1,7 @@
+# Update the box
+#Everything here will probably be already installed but it doesn't hurt to check...
+apt-get -y update
+apt-get -y install linux-headers-$(uname -r) build-essential
+apt-get -y install zlib1g-dev libssl-dev libreadline-gplv2-dev sudo
+apt-get -y install curl unzip
+
