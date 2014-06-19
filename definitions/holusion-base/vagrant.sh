7fcb2e2b02893a56d74021343e2764de0792d1ab
Sebastien Dumetz <sebastien.dumetz@isen-lille.fr>
2014-06-19 16:05:28 +0200

    changed dir for vagrant ssh keys to correct home dir

--- definitions/holusion-base/vagrant.sh
+++ definitions/holusion-base/vagrant.sh
@@ -1,20 +1,20 @@
 # Set up Vagrant.
 
 date > /etc/vagrant_box_build_time
-
+HOME=/home/vagrant/
 # Create the user vagrant with password vagrant
-useradd -G sudo -p $(perl -e'print crypt("vagrant", "vagrant")') -m -s /bin/bash -N vagrant -d /home/vagrant
+useradd -G sudo -p $(perl -e'print crypt("vagrant", "vagrant")') -m -s /bin/bash -N vagrant -d $HOME
 
 #Make sure sshd doesn't start before postinstall is finished!
 
 sed -i '/# Required-Start:/c\#  Required-Start: $all' /etc/init.d/ssh
 
 # Install vagrant keys
-mkdir -pm 700 /vagrant/.ssh
-curl -Lo /vagrant/.ssh/authorized_keys \
+mkdir -pm 700 $HOME/.ssh
+curl -Lo $HOME/.ssh/authorized_keys \
   'https://raw.github.com/mitchellh/vagrant/master/keys/vagrant.pub'
-chmod 0600 /vagrant/.ssh/authorized_keys
-chown -R vagrant /vagrant/.ssh
+chmod 0600 $HOME/.ssh/authorized_keys
+chown -R vagrant $HOME/.ssh
 
 # Customize the message of the day
 echo 'Welcome to your Vagrant-built virtual machine.' > /var/run/motd
