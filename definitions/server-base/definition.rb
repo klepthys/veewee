4ee560312330d125f39c902550b8ffd941ab3105
Sebastien Dumetz <sebastien.dumetz@isen-lille.fr>
2014-07-10 10:47:19 +0200

    added a vagrant base server installation

---
+++ definitions/server-base/definition.rb
@@ -0,0 +1,50 @@
+#Thanks to Fletcher Nichol - https://github.com/fnichol
+
+Veewee::Definition.declare({
+  :cpu_count => '1',
+  :memory_size=> '256',
+  :disk_size => '10140', :disk_format => 'VDI', :hostiocache => 'off',
+  :os_type_id => 'Debian_64',
+  :iso_file => "holusion_installer.iso",
+  :iso_src => "http://dev.holusion.net/development/holusion_installer.iso",
+  :iso_download_timeout => "1000",
+  :boot_wait => "10", :boot_cmd_sequence => [
+     '<Esc>',
+     'install ',
+     'preseed/url=http://%IP%:%PORT%/preseed.cfg ',
+     'debian-installer=en_US ',
+     'auto ',
+     'locale=fr_FR ',
+     'kbd-chooser/method=fr ',
+     'netcfg/get_hostname=%NAME% ',
+     'netcfg/get_domain=vagrantup.com ',
+     'fb=false ',
+     'debconf/frontend=noninteractive ',
+     'console-setup/ask_detect=false ',
+     'console-keymaps-at/keymap=fr ',
+     'keyboard-configuration/xkb-keymap=fr ',
+     '<Enter>'
+  ],
+  :kickstart_port => "7122",
+  :kickstart_timeout => "10000",
+  :kickstart_file => "preseed.cfg",
+  :ssh_login_timeout => "10000",
+  :ssh_user => "vagrant",
+  :ssh_password => "vagrant",
+  :ssh_key => "",
+  :ssh_host_port => "7222",
+  :ssh_guest_port => "22",
+  :sudo_cmd => "echo '%p'|sudo -S bash '%f'",
+  :shutdown_cmd => "halt -p",
+  :postinstall_files => [
+    "base.sh",
+    "vagrant.sh",
+    "virtualbox.sh",
+    #"vmfusion.sh",
+    "ruby.sh",
+    "puppet.sh",
+    "cleanup.sh",
+    "zerodisk.sh"
+  ],
+  :postinstall_timeout => "10000"
+})
